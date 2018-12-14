14e4be5d37405da1987eec3f66fb8fffc505cfea
YoussefRamoud <youssef.ramoud@student.guc.edu.eg>
2018-12-14 20:53:30 +0200

    waiting for frontend

---
+++ website/view.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>View Records</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body>
+
+<?php
+// connect to the database
+include('server.php');
+
+// get the records from the database
+if ($result = $db->query("SELECT * FROM users"))
+{
+// display records if there are records to display
+if ($result->num_rows > 0)
+{
+// display records in a table
+echo "<table border='1' cellpadding='10'>";
+
+// set table headers
+echo "<tr><th>name</th><th>email</th><th>type</th></tr>";
+
+while ($row = $result->fetch_object())
+{
+// set up a row for each record
+echo "<tr>";
+echo "<td>" . $row->name . "</td>";
+echo "<td>" . $row->email . "</td>";
+echo "<td>" . $row->type . "</td>";
+//echo "<td><a href='records.php?id=" . $row->id . "'>Edit</a></td>";
+//echo "<td><a href='delete.php?id=" . $row->id . "'>Delete</a></td>";
+echo "</tr>";
+}
+
+echo "</table>";
+}
+// if there are no records in the database, display an alert message
+else
+{
+echo "No results to display!";
+}
+}
+// show an error if there is an issue with the database query
+else
+{
+echo "Error: " . $mysqli->error;
+}
+
+// close database connection 
+$db->close();
+//<a href="records.php">Add New Record</a> mafrood tb2a taht
+?>
+
+
+</body>
+</html>
